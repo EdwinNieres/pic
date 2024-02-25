41d24b2fe0d6a154d27e3b74445764aac0f481d7
Edwin Nieres <ceosbtc@gmail.com>
2024-02-25 13:27:53 -0300

    añadiendo los archivos

---
+++ src/views/Home.jsx
@@ -0,0 +1,13 @@
+import Galeria from "../components/Galeria"; // importacion componente galeria
+import MyContext from "../MyContext";
+import { useContext } from "react";
+
+export default function Home() {
+  return (
+    <div id="Home">
+      <h1 className="title">Natural Pic</h1>
+
+      <Galeria />
+    </div>
+  );
+}
